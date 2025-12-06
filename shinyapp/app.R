95cc57083c4758c76396e3d89911af4266e909ca
Kebededebbie5 <kebededebbie@gmail.com>
2025-12-05 20:55:20 -0500

    I changed the title above the top graph in the Event Summary tab from 'Level Summary of Team 118' to 'Event Summary for all teams'

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -294,7 +294,7 @@ server <- function(input, output, session) {
 
         ggplot(data, aes(x = team_number, y = points_score, fill = type)) +
             geom_bar(position = "stack", stat = "identity") +
-            labs(title = paste("Level Summary for Team", data$team_number), x = "Team", y = "Score", fill = "Points") + 
+            labs(title = "Event Summary for all teams") + 
             scale_fill_manual(
                 values = c(
                     "plum1", "plum3", "plum4", "#FFF68F", "#FFC156", "#3D5A80",
